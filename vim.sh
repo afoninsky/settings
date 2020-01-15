b8f3e1340b05891488e584e11bd830f8d734dbee
Andrey Afoninskiy <andrey.afoninskiy@mambu.com>
2020-01-15 09:45:40 +0100

    .

--- vim.sh
+++ vim.sh
@@ -1,14 +1,16 @@
 #!env sh
+brew install coreutils
+
 ln -s `realpath .`/vim/ ~/.vim/custom
 ln -s `realpath .`/vim/vimrc ~/.vimrc
 
-git clone --depth 1 https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
+# git clone --depth 1 https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
 
-git clone --depth 1 https://github.com/scrooloose/nerdtree.git ~/.vim/pack/dist/start/nerdtree
+# git clone --depth 1 https://github.com/scrooloose/nerdtree.git ~/.vim/pack/dist/start/nerdtree
 
 git clone --depth 1 https://github.com/vim-airline/vim-airline ~/.vim/pack/dist/start/vim-airline
 
-git clone --depth 1 https://github.com/aserebryakov/vim-todo-lists.git ~/.vim/pack/dist/start/vim-todo-list
+# git clone --depth 1 https://github.com/aserebryakov/vim-todo-lists.git ~/.vim/pack/dist/start/vim-todo-list
 
 #curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
 #    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- zsh.sh
+++ zsh.sh
@@ -1,8 +1,9 @@
 #!env sh
+brew install zsh coreutils
+
 ln -s `realpath .`/zsh/ ~/.zsh
 ln -s `realpath .`/zsh/zshrc ~/.zshrc
 
-brew install zsh coreutils
 chsh -s /bin/zsha
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
