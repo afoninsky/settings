4ccf7014c0703c1c1113bdf55b93ded98552a8bc
Andrey Afoninskiy <andrey.afoninskiy@mambu.com>
2020-02-11 09:48:22 +0100

    add custom envs

--- zsh.sh
+++ zsh.sh
@@ -1,9 +1,12 @@
 #!env sh
-brew install zsh coreutils
+# brew install zsh coreutils
+
+rm -R ~/.zsh
+rm  ~/.zshrc
 
 ln -s `realpath .`/zsh/ ~/.zsh
 ln -s `realpath .`/zsh/zshrc ~/.zshrc
 
-chsh -s /bin/zsha
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# chsh -s /bin/zsha
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
